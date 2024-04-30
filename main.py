2762c5beb006fef37dec41432bb6cdf251675873
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-04-30 10:51:35 +1000

    commit

--- main.py
+++ main.py
@@ -40,26 +40,30 @@ while not GAME.EXIT:
 
     GAME.SCREEN.blit(BACKGROUND_IMAGE, (0,0))
 
+    #Start Game Screen
     if GAME.STATE == "Start Game":
         text = FONT.render("Press SPACE to Start", True, (255,0,0) )
         GAME.SCREEN.blit(text, (280,320))
 
+        #get the mouse position
         x = mouse_pos.x
         y = mouse_pos.y
         position_text = "X: " + str(x) + " Y: " + str(y)
         text = FONT2.render(position_text, True, (255,255,255))
         GAME.SCREEN.blit(text, (10,10))
 
+        #test to see if space is pressed
         if pressed[pygame.K_SPACE] == 1:
             GAME.STATE = "Running"
             GAME.MUSIC = pygame.mixer.Sound("sounds/sunsetreverie.mp3")
             GAME.MUSIC.play(-1) 
 
+    #Running Game Screen
     elif GAME.STATE == "Running":
         text = FONT2.render("Score: 0", True, (255,0,0) )
         GAME.SCREEN.blit(text, (10,10))
 
-        if pressed[pygame.K_q] == 1:
+        if pressed[pygame.K_q] == 1:    #quit the game if q is pressed
             GAME.EXIT = True
 
     pygame.display.flip() #all drawing that was done off screen is now flipped onto the screen
